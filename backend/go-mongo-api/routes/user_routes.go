a0e8824db0ecd45a1db4267f6e64c4e729c8fe78
Ben Phoebus <z5313164@student.unsw.edu.au>
2022-10-04 14:40:53 +1100

    working userRoutes crud app

--- backend/go-mongo-api/routes/user_routes.go
+++ backend/go-mongo-api/routes/user_routes.go
@@ -1,7 +1,15 @@
 package routes
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/gorilla/mux"
+	"go-mongo-api/controllers"
+) 
 
 func UserRoute(router *mux.Router) {
 	// All routes related to users comes here
+	router.HandleFunc("/user", controllers.CreateUser()).Methods("POST")
+	router.HandleFunc("/user/{userId}", controllers.GetAUser()).Methods("GET")
+	router.HandleFunc("/user/{userId}", controllers.EditAUser()).Methods("PUT")
+	router.HandleFunc("/user/{userId}", controllers.DeleteAUser()).Methods("DELETE")
+	router.HandleFunc("/users", controllers.GetAllUser()).Methods("GET")
 }
